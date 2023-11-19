01a60ef60a24f1c69da38d7d21cf6f35814b3583
Natanon Trangratanajit <nutt.nonn@gmail.com>
2023-11-19 14:58:36 +0700

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,13 @@ function App() {
     });
   }
 
-  function handleDeleteTask() {
-
+  function handleDeleteTask(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
   }
 
   function handleSelectProject(id) {
@@ -108,6 +113,7 @@ function App() {
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
